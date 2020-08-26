ac12a21b12f9df0db560b29ae3a223eaa4b495d0
faiana <biafriedrich06@gmail.com>
2020-08-26 23:37:17 +0100

    prod api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://hitech-health-employees-api.herokuapp.com'
+  // apiUrl: 'http://localhost:8080'
 
 };
 
